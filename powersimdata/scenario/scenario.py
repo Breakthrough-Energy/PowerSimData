353f31b796d0d9932625eb4c3d6ce6b4e5e2d8a3
Ben Rouille d'Orfeuil <brdo@intven.com>
2019-01-17 14:56:02 -0800

    feat: Write constructor and _check_scenario private method

--- powersimdata/scenario/scenario.py
+++ powersimdata/scenario/scenario.py
@@ -1,6 +1,25 @@
+from postreise.process.transferdata import TransferData
+
 class Scenario():
     """Retrieve information related a scenario
 
     :param str name: name of scenario.
 
     """
+
+    def __init__(self, name):
+        self.name = name
+        
+        # Check scenario
+        self._check_scenario()
+
+    def _check_scenario(self):
+        td = TransferData()
+        scenarios = td.get_scenario_list()
+        if self.name not in scenarios:
+            print("Scenario not available. Possible scenarios are:")
+            for s in scenarios:
+                print(s)
+            return
+
+        
