07e684f20f323898eab1c8ca6735788a880ea74e
rouille <brdo@intven.com>
2019-01-28 14:31:55 -0800

    feat: Add change table

--- powersimdata/input/profiles.py
+++ powersimdata/input/profiles.py
@@ -1,4 +1,5 @@
 import os
+import pickle
 import pandas as pd
 from pathlib import Path
 from postreise.process.transferdata import PullData
@@ -6,9 +7,9 @@ from postreise.process.transferdata import PullData
 
 class InputData(object):
     """Input Data class.
-        This class enables you to download data from the server as well as \ 
-        from a local folder. The :meth:`~get_data` function will first look \ 
-        locally if it can find the data requested. If it can't find locally \ 
+        This class enables you to download data from the server as well as \
+        from a local folder. The :meth:`~get_data` function will first look \
+        locally if it can find the data requested. If it can't find locally \
         it will download it from the server if it can find it there.
 
     :param str local_dir: define local folder location to read or save data.
@@ -32,20 +33,21 @@ class InputData(object):
         :param str scenario_name: name of scenario to get data from.
         :param str field_name: *'demand'*, *'hydro'*, *'solar'* or *'wind'*.
         :return: (*pandas*) -- data frame of demand, hydro, solar or wind.
-        :raises FileNotFoundError: file found neither locally nor on the \ 
+        :raises FileNotFoundError: file found neither locally nor on the \
             server.
-        :raises NameError: If type not *'demand'*, *'hydro'*, *'solar'* or \ 
-            *'wind'*.
+        :raises NameError: If type not *'demand'*, *'hydro'*, *'solar'*, \
+            *'wind'* or *'ct'*.
         """
-        if field_name not in ['demand', 'hydro', 'solar', 'wind']:
-            raise NameError('Can only get demand, hydro, solar or wind data.')
+        if field_name not in ['demand', 'hydro', 'solar', 'wind', 'ct']:
+            raise NameError('Can only get demand, hydro, solar, wind and',
+                            'change table data.')
         try:
             p_out = pd.read_pickle(
                 self.local_dir + scenario_name + '_' + field_name + '.pkl'
             )
         except FileNotFoundError:
-            print('Local file not found will',
-                  'download data from server and save locally.')
+            print('Local file not found. Data will be downloaded from',
+                  'server and saved locally.')
             try:
                 p_out = self.TD.download(scenario_name, field_name)
             except FileNotFoundError as e:
@@ -55,7 +57,11 @@ class InputData(object):
             if not os.path.exists(self.local_dir):
                 os.makedirs(self.local_dir)
             print('Saving file locally.')
-            p_out.to_pickle(
-                self.local_dir + scenario_name + '_' + field_name + '.pkl')
+            file_name = self.local_dir + scenario_name + '_' + \
+                        field_name + '.pkl'
+            if field_name == 'ct':
+                pickle.dump(p_out, open(file_name, "wb"))
+            else:
+                p_out.to_pickle(file_name)
 
         return p_out
