8610a951e36f17bb36b2f9d34cccc95dce12673d
Ben RdO <brdo@intven.com>
2019-03-21 13:40:00 -0700

    style: Change print message

--- powersimdata/scenario/state.py
+++ powersimdata/scenario/state.py
@@ -12,10 +12,10 @@ class State(object):
         :param class state: One of the sub-classes.
         """
         if state.name in self.allowed:
-            print('Switching from <%s> to <%s>' % (self, state.name))
+            print('%s --> %s' % (self, state.name))
             self.__class__ = state
         else:
-            print('Cannot switch from <%s> to <%s>' % (self, state.name))
+            print('%s -/-> %s' % (self, state.name))
 
     def __str__(self):
         return self.name
