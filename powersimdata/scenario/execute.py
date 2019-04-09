55742cf06376b1dc2e1996c5e250b0be672e678d
Ben RdO <brdo@intven.com>
2019-04-09 14:11:30 -0700

    feat: Add print_scenario_info method

--- powersimdata/scenario/execute.py
+++ powersimdata/scenario/execute.py
@@ -13,4 +13,16 @@ class Execute(State):
         """Initializes attributes.
 
         """
-        pass
+        self._scenario_info = scenario._info
+        print("SCENARIO: %s | %s" % (self._scenario_info['plan'],
+                                     self._scenario_info['name']))
+
+    def print_scenario_info(self):
+        """Prints scenario information.
+
+        """
+        print("--------------------")
+        print("SCENARIO INFORMATION")
+        print("--------------------")
+        for key, val in self._scenario_info.items():
+            print("%s: %s" % (key, val))
