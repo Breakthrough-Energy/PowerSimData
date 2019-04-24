16f421ca12a4156ff11c3dc4e3c52f8af2da1af8
Ben RdO <brdo@intven.com>
2019-04-24 15:52:31 -0700

    feat: Create PrepareScenario class

--- powersimdata/scenario/execute.py
+++ powersimdata/scenario/execute.py
@@ -1,6 +1,7 @@
 from postreise.process import const
 from postreise.process.transferdata import setup_server_connection
 from postreise.process.transferdata import PullData
+from powersimdata.input.scaler import Scaler
 from powersimdata.input.grid import Grid
 from powersimdata.scenario.state import State
 
@@ -58,25 +59,27 @@ class Execute(State):
         """Prepares scenario for execution
 
         """
-        print("PREPARING SCENARIO:\n")
-        self._update_execute_list('preparing')
-        self._create_folder()
-        self._copy_input_file()
+        self._update_scenario_status()
+        if self._scenario_status == 'ready':
+            print("PREPARING SCENARIO:\n")
+
+            self._scaler = Scaler(self._scenario_info)
+
+            ps = PrepareScenario(self._scaler)
+            #ps.create_folder()
+            #if self._scaler.ct is not None:
+            #    ps.scale_input()
+            #else:
+            #    ps.copy_input_file()
+            #self._update_execute_list('prepared')
+        else:
+            print("-------------------------")
+            print("SCENARIO ALREADY PREPARED")
+            print("-------------------------")
+            print("Current status: %s" % self._scenario_status)
+            return
 
 
-    def _create_folder(self):
-        """Creates folder on server that will enclose simulation inputs.
-
-        :raises IOError: if folder cannot be created.
-        """
-        print("--> Creating temporary folder on server")
-        self._tmp_dir = '%s/scenario_%s' % (const.EXECUTE_DIR,
-                                            self._scenario_info['id'])
-        command = "mkdir %s" % self._tmp_dir
-        ssh = setup_server_connection()
-        stdin, stdout, stderr = ssh.exec_command(command)
-        if len(stderr.readlines()) != 0:
-            raise IOError("Failed to create %s on server" % self._tmp_dir)
 
     def _update_execute_list(self, status):
         """Updates status in execute list file on server.
@@ -94,16 +97,51 @@ class Execute(State):
         if len(stderr.readlines()) != 0:
             raise IOError("Failed to update %s on server" % const.EXECUTE_LIST)
 
-    def _copy_input_file(self):
+
+class PrepareScenario(object):
+    """Prepares scenario for execution.
+
+    """
+
+    def __init__(self, scaler):
+        """Initializes attributes.
+
+        :param object scaler: Scaler instance.
+        """
+        self._scaler = scaler
+        self._tmp_dir = '%s/scenario_%s' % (const.EXECUTE_DIR,
+                                            scaler.scenario_id)
+
+    def create_folder(self):
+        """Creates folder on server that will enclose simulation inputs.
+
+        :raises IOError: if folder cannot be created.
+        """
+        print("--> Creating temporary folder on server")
+        command = "mkdir %s" % self._tmp_dir
+        ssh = setup_server_connection()
+        stdin, stdout, stderr = ssh.exec_command(command)
+        if len(stderr.readlines()) != 0:
+            raise IOError("Failed to create %s on server" % self._tmp_dir)
+
+    def copy_input_file(self):
         """Copies simulation inputs in temporary directory on server.
 
         :raises IOError: if files cannot be copied.
         """
         print("--> Copying files into folder")
         command = "cp -a %s/%s_* %s" % (const.INPUT_DIR,
-                                        self._scenario_info['id'],
+                                        self._scaler.scenario_id,
                                         self._tmp_dir)
         ssh = setup_server_connection()
         stdin, stdout, stderr = ssh.exec_command(command)
         if len(stderr.readlines()) != 0:
             raise IOError("Failed to copy inputs on server" % self._tmp_dir)
+
+    def scale_input(self):
+        """Scales simulation input data.
+
+        :raises IOError: if files cannot be copied.
+        """
+        print("--> Scaling input data")
+        self.grid = self._scaler.get_grid()
