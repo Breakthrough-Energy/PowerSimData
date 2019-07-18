1bff78059afb62f45660c0cedba54b601caaf675
Ben RdO <brdo@intven.com>
2019-07-18 12:54:45 -0700

    chore: Set version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='powersimdata',
-      version='0.1',
+      version='0.3',
       description='Power Simulation Data',
       url='https://github.com/intvenlab/powersimdata',
       author='Kaspar Mueller',
