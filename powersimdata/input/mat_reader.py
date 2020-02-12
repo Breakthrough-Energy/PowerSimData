8e289bc2ae4edee2cff0b4751a655f780535bd9e
Ben RdO <brdo@intven.com>
2020-02-11 16:06:23 -0800

    refactor: Use common function to build data frame

--- powersimdata/input/mat_reader.py
+++ powersimdata/input/mat_reader.py
@@ -32,85 +32,118 @@ class MATReader(AbstractGrid):
 
     def _build_network(self):
         data = loadmat(self.data_loc, squeeze_me=True, struct_as_record=False)
-        self.plant = get_plant(data['mdi'].mpc.gen,
-                               data['mdi'].mpc.genid)
-        self.branch = get_branch(data['mdi'].mpc.branch,
-                                 data['mdi'].mpc.branchid)
+        self.plant = build_data_frame('plant',
+                                      data['mdi'].mpc.gen,
+                                      data['mdi'].mpc.genid)
+
+        self.branch = build_data_frame('branch',
+                                       data['mdi'].mpc.branch,
+                                       data['mdi'].mpc.branchid)
+
+        self.gencost['after'] = build_data_frame('gencost_after',
+                                                 data['mdi'].mpc.gencost,
+                                                 data['mdi'].mpc.genid)
         """
-        self.bus = get_bus(data['mdi'].mpc.bus, 
-                           data['mdi'].mpc.busid)
-        self.dcline = get_dcline(data['mdi'].mpc.dcline, 
-                           data['mdi'].mpc.dclineid)
-        self.gencost['before'] = get_gencost(data['mdi'].mpc.gencost_before,
-                                             data['mdi'].mpc.genid)
-        self.gencost['after'] = get_gencost(data['mdi'].mpc.gencost_after,
-                                            data['mdi'].mpc.genid),
+        self.sub = build_data_frame('sub',
+                                    data['mdi'].mpc.sub,
+                                    data['mdi'].mpc.subid)
+
+        self.bus = build_data_frame('bus',
+                                    data['mdi'].mpc.bus,
+                                    data['mdi'].mpc.busid)
+
+        self.bus2sub = build_data_frame('bus2sub',
+                                        data['mdi'].mpc.bus2sub,
+                                        data['mdi'].mpc.busid)
+
+        self.dcline = build_data_frame('dcline',
+                                       data['mdi'].mpc.dcline,
+                                       data['mdi'].mpc.dclineid)
+
+        self.gencost['before'] = build_data_frame('gencost_before',
+                                                  data['mdi'].mpc.gencost_orig,
+                                                  data['mdi'].mpc.genid)
+        self.interconnect = data['mdi'].mpc.interconnect
+        self.id2zone = {k: self.id2zone[k] for k in self.bus.zone_id.unique()}
+        self.zone2id = {value: key for key, value in self.id2zone.items()}
         """
 
 
-def get_bus(table, index):
-    """Sets bus data frame.
+def build_data_frame(name, table, index):
+    """Builds data frame from MAT-file.
 
-    :param numpy.array table: bus table enclosed in MAT-file.
-    :param numpy.array index: array of bus ids enclosed in MAT-file.
-    :return: (pandas.DataFrame) -- bus data frame.
+    :param str name: data frame name.
+    :param numpy.array table: table to be used to generate data frame.
+    :param numpy.array index: array to be used as data frame indices.
+    :return: (pandas.DataFrame) -- data frame.
+    :raises ValueError: if name does not exist and table has wrong shape.
     """
-    col_name = [
+    if name not in index_name_provider().keys():
+        raise ValueError('Unknown %s table' % name)
+
+    print('Loading %s' % name)
+    if name.split('_')[0] == 'gencost':
+        if table.shape[0] == index.shape[0]:
+            data_frame = pd.DataFrame(table, index=index)
+            data_frame = format_gencost(data_frame)
+        else:
+            raise ValueError('Length of %s is %s. Indices imply %s' %
+                             (name, table.shape[0], index.shape[0]))
+    else:
+        col_name = column_name_provider()[name]
+        expected_shape = (index.shape[0], len(col_name))
+        if table.shape == expected_shape:
+            data_frame = pd.DataFrame(table, columns=col_name, index=index)
+        else:
+            raise ValueError('Shape of %s is %s. Indices and columns imply %s' %
+                             (name, table.shape, expected_shape))
+
+    data_frame.index.name = index_name_provider()[name]
+    return data_frame
+
+
+def index_name_provider():
+    """Provides index name for data frame.
+
+    :return: (*dict*) -- dictionary of data frame index name.
+    """
+    index_name = {'bus': 'bus_id',
+                  'branch': 'branch_id',
+                  'dcline': 'dcline_id',
+                  'plant': 'plant_id',
+                  'gencost_before': 'plant_id',
+                  'gencost_after': 'plant_id'}
+    return index_name
+
+
+def column_name_provider():
+    """Provides column names for data frame.
+
+    :return: (*dict*) -- dictionary of data frame columns name.
+    """
+    col_name_bus = [
         'type', 'Pd', 'Qd', 'Gs', 'Bs', 'zone_id', 'Vm', 'Va', 'loss_zone',
         'baseKV', 'Vmax', 'Vmin', 'lam_P', 'lam_Q', 'mu_Vmax', 'mu_Vmin']
-    bus = pd.DataFrame(table, columns=col_name, index=index)
-    bus.index.name = 'bus_id'
-    return bus
-
-
-def get_branch(table, index):
-    """Sets branch data frame.
-
-    :param numpy.array table: branch table enclosed in MAT-file.
-    :param numpy.array index: array of branch ids enclosed in MAT-file.
-    :return: (pandas.DataFrame) -- branch data frame.
-    """
-    col_name = [
+    col_name_branch = [
         'from_bus_id', 'to_bus_id', 'r', 'x', 'b', 'rateA', 'rateB', 'rateC',
         'ratio', 'angle', 'status', 'angmin', 'angmax', 'Pf', 'Qf', 'Pt', 'Qt',
         'mu_Sf', 'mu_St', 'mu_angmin', 'mu_angmax']
-    branch = pd.DataFrame(table, columns=col_name, index=index)
-    branch.index.name = 'bus_id'
-    return branch
-
-
-def get_dcline(table, index):
-    """Sets dcline data frame.
-
-    :param numpy.array table: dcline table enclosed in MAT-file.
-    :param numpy.array index: array of dcline ids enclosed in MAT-file.
-    :return: (pandas.DataFrame) -- dcline data frame.
-    """
-    col_name = [
+    col_name_dcline = [
         'from_bus_id', 'to_bus_id', 'status', 'Pf', 'Pt', 'Qf', 'Qt', 'Vf',
         'Vt', 'Pmin', 'Pmax', 'QminF', 'QmaxF', 'QminT', 'QmaxT', 'loss0',
         'loss1', 'muPmin', 'muPmax', 'muQminF', 'muQmaxF', 'muQminT',
         'muQmaxT']
-    dcline = pd.DataFrame(table, columns=col_name, index=index)
-    dcline.index.name = 'dcline_id'
-    return dcline
-
-
-def get_plant(table, index):
-    """Sets plant data frame.
-
-    :param numpy.array table: plant table enclosed in MAT-file.
-    :param numpy.array index: array of plant ids enclosed in MAT-file.
-    :return: (pandas.DataFrame) -- plant data frame.
-    """
-    col_name = [
+    col_name_plant = [
         'bus_id', 'Pg', 'Qg', 'Qmax', 'Qmin', 'Vg', 'mBase', 'status', 'Pmax',
         'Pmin', 'Pc1', 'Pc2', 'Qc1min', 'Qc1max', 'Qc2min', 'Qc2max',
         'ramp_agc', 'ramp_10', 'ramp_30', 'ramp_q', 'apf', 'mu_Pmax', 'mu_Pmin',
         'mu_Qmax', 'mu_Qmin']
-    plant = pd.DataFrame(table, columns=col_name, index=index)
-    plant.index.name = 'plant_id'
-    return plant
+    col_name = {'bus': col_name_bus,
+                'branch': col_name_branch,
+                'dcline': col_name_dcline,
+                'plant': col_name_plant,
+                }
+    return col_name
 
 
 def get_gencost(table, index):
