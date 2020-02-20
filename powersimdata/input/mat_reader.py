bdbcff5a3c981b5c0134c7cf941c4427b6205564
Ben RdO <brdo@intven.com>
2020-02-19 21:09:54 -0800

    feat: Read sub, bus, bus2sub, dcline, zone2id, id2zone and interconnect from MAT-file

--- powersimdata/input/mat_reader.py
+++ powersimdata/input/mat_reader.py
@@ -27,89 +27,108 @@ class MATReader(AbstractGrid):
         :raises IOError: if file does not exist.
         """
         if os.path.isfile(filename) is False:
-            raise IOError('%s file not found' % filename)
+            raise FileNotFoundError('%s file not found' % filename)
         else:
             self.data_loc = filename
 
     def _build_network(self):
         data = loadmat(self.data_loc, squeeze_me=True, struct_as_record=False)
 
+        self.storage = get_storage()  # dict of empty data frame
+
+        try:
+            n_storage = data['mdi'].mpc.iess.shape[0]
+        except AttributeError:
+            n_storage = 0
+
+        try:
+            n_dcline = data['mdi'].mpc.dclineid.shape[0]
+        except AttributeError:
+            n_dcline = 0
+
+        # bus
+        self.bus, _ = frame('bus',
+                            data['mdi'].mpc.bus,
+                            data['mdi'].mpc.busid)
+        # plant
+        self.plant, storage_gen = frame('plant',
+                                        data['mdi'].mpc.gen,
+                                        data['mdi'].mpc.genid,
+                                        n_storage=n_storage)
+        # gencost before
+        self.gencost['before'], _ = frame('gencost_before',
+                                          data['mdi'].mpc.gencost_orig,
+                                          data['mdi'].mpc.genid,
+                                          n_storage=n_storage)
+        # gencost after
+        self.gencost['after'], storage_gencost = frame('gencost_after',
+                                                       data['mdi'].mpc.gencost,
+                                                       data['mdi'].mpc.genid,
+                                                       n_storage=n_storage)
+        # branch
         self.branch, _ = frame('branch',
                                data['mdi'].mpc.branch,
                                data['mdi'].mpc.branchid)
-        self.plant, storage_gen = frame('plant',
-                                        data['mdi'].mpc.gen,
-                                        data['mdi'].mpc.genid)
-        self.gencost['after'], storage_gencost = frame('gencost_after',
-                                                       data['mdi'].mpc.gencost,
-                                                       data['mdi'].mpc.genid)
-
-        if storage_gen is not None:
-            self.storage = get_storage()
+        # DC line
+        if n_dcline > 0:
+            self.dcline, _ = frame('dcline',
+                                   data['mdi'].mpc.dcline,
+                                   data['mdi'].mpc.dclineid)
+        # substation
+        self.sub, _ = frame('sub',
+                            data['mdi'].mpc.sub,
+                            data['mdi'].mpc.subid)
+        # bus to sub mapping
+        # self.bus2sub, _ = frame('bus2sub',
+        #                        data['mdi'].mpc.bus2sub,
+        #                        data['mdi'].mpc.busid)
+        # zone
+        self.zone2id = link(data['mdi'].mpc.zonename, data['mdi'].mpc.zoneid)
+        self.id2zone = link(data['mdi'].mpc.zoneid, data['mdi'].mpc.zonename)
+        # storage
+        if n_storage > 0:
             self.storage['gen'] = storage_gen
             self.storage['gencost'] = storage_gencost
             col_name = self.storage['StorageData'].columns
             for c in col_name:
                 self.storage['StorageData'][c] = eval('data["mdi"].Storage.'+c)
-        else:
-            self.storage = get_storage()
-
-        """
-        self.sub, _ = frame('sub',
-                            data['mdi'].mpc.sub,
-                            data['mdi'].mpc.subid)
-
-        self.bus, _ = frame('bus',
-                            data['mdi'].mpc.bus,
-                            data['mdi'].mpc.busid)
-
-        self.bus2sub, _ = frame('bus2sub',
-                                data['mdi'].mpc.bus2sub,
-                                data['mdi'].mpc.busid)
-
-        self.dcline, _ = frame('dcline',
-                               data['mdi'].mpc.dcline,
-                               data['mdi'].mpc.dclineid)
-
-        self.gencost['before'], _ = frame('gencost_before',
-                                          data['mdi'].mpc.gencost_orig,
-                                          data['mdi'].mpc.genid)
-        self.interconnect = data['mdi'].mpc.interconnect
-        self.id2zone = {k: self.id2zone[k] for k in self.bus.zone_id.unique()}
-        self.zone2id = {value: key for key, value in self.id2zone.items()}
-        """
+        # interconnect
+        self.interconnect = self.sub.interconnect.unique().tolist()
 
 
-def frame(name, table, index):
+def frame(name, table, index, n_storage=0):
     """Builds data frame from MAT-file.
 
     :param str name: structure name.
     :param numpy.array table: table to be used to build data frame.
     :param numpy.array index: array to be used as data frame indices.
+    :param int n_storage: number of storage devices.
     :return: (tuple) -- first element is a data frame. Second element is None
         or a data frame when energy storage system are included.
     :raises ValueError: if name does not exist and table has wrong shape.
     """
+    data = pd.DataFrame()
     storage = None
     print('Loading %s' % name)
     if name.split('_')[0] == 'gencost':
         if table.shape[0] == index.shape[0]:
             data = format_gencost(pd.DataFrame(table, index=index))
-        else:
-            data = format_gencost(pd.DataFrame(table[:index.shape[0]],
-                                               index=index))
-            storage = format_gencost(pd.DataFrame(table[index.shape[0]:]))
+        elif n_storage > 0:
+            data = format_gencost(
+                pd.DataFrame(table[:index.shape[0]], index=index))
+            storage = format_gencost(
+                pd.DataFrame(table[index.shape[0]:index.shape[0]+n_storage]))
     elif name in ['branch', 'bus', 'bus2sub', 'dcline', 'plant', 'sub']:
         col_name = column_name_provider()[name]
         expected_shape = (index.shape[0], len(col_name))
         if table.shape == expected_shape:
             data = pd.DataFrame(table, columns=col_name, index=index)
-        else:
-            data = pd.DataFrame(table[:index.shape[0]],
-                                columns=col_name,
-                                index=index)
-            storage = pd.DataFrame(table[index.shape[0]:],
-                                   columns=col_name)
+        elif n_storage > 0:
+            data = pd.DataFrame(
+                table[:index.shape[0]], columns=col_name, index=index)
+            storage = pd.DataFrame(
+                table[index.shape[0]:index.shape[0]+n_storage:],
+                columns=col_name)
     else:
         raise ValueError('Unknown %s table' % name)
 
@@ -117,12 +136,24 @@ def frame(name, table, index):
     return data, storage
 
 
+def link(keys, values):
+    """Creates hash table
+
+    :param numpy.array keys: key.
+    :param numpy.array values: value.
+    :return: (*dict*) -- hash table.
+    """
+    return {k: v for k, v in zip(keys, values)}
+
+
 def index_name_provider():
     """Provides index name for data frame.
 
     :return: (*dict*) -- dictionary of data frame index name.
     """
-    index_name = {'bus': 'bus_id',
+    index_name = {'sub': 'sub_id',
+                  'bus': 'bus_id',
+                  'bus2sub': 'bus_id',
                   'branch': 'branch_id',
                   'dcline': 'dcline_id',
                   'plant': 'plant_id',
@@ -136,9 +167,13 @@ def column_name_provider():
 
     :return: (*dict*) -- dictionary of data frame columns name.
     """
+    col_name_sub = [
+        'name', 'interconnect_sub_id', 'lat', 'lon', 'interconnect']
     col_name_bus = [
-        'type', 'Pd', 'Qd', 'Gs', 'Bs', 'zone_id', 'Vm', 'Va', 'loss_zone',
-        'baseKV', 'Vmax', 'Vmin', 'lam_P', 'lam_Q', 'mu_Vmax', 'mu_Vmin']
+        'bus_id', 'type', 'Pd', 'Qd', 'Gs', 'Bs', 'zone_id', 'Vm', 'Va',
+        'loss_zone', 'baseKV', 'Vmax', 'Vmin', 'lam_P', 'lam_Q', 'mu_Vmax',
+        'mu_Vmin']
+    col_name_bus2sub = ['sub_id', 'interconnect']
     col_name_branch = [
         'from_bus_id', 'to_bus_id', 'r', 'x', 'b', 'rateA', 'rateB', 'rateC',
         'ratio', 'angle', 'status', 'angmin', 'angmax', 'Pf', 'Qf', 'Pt', 'Qt',
@@ -153,11 +188,13 @@ def column_name_provider():
         'Pmin', 'Pc1', 'Pc2', 'Qc1min', 'Qc1max', 'Qc2min', 'Qc2max',
         'ramp_agc', 'ramp_10', 'ramp_30', 'ramp_q', 'apf', 'mu_Pmax', 'mu_Pmin',
         'mu_Qmax', 'mu_Qmin']
-    col_name = {'bus': col_name_bus,
-                'branch': col_name_branch,
-                'dcline': col_name_dcline,
-                'plant': col_name_plant,
-                }
+    col_name = {
+        'sub': col_name_sub,
+        'bus': col_name_bus,
+        'bus2sub': col_name_bus2sub,
+        'branch': col_name_branch,
+        'dcline': col_name_dcline,
+        'plant': col_name_plant}
     return col_name
 
 
