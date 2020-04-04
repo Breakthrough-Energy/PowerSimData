abe64080c3b51556e237ed40aab2d1f5d28e3c93
Ben RdO <brdo@intven.com>
2020-04-04 14:31:23 -0700

    refactor: use extract_data module located in REISE

--- powersimdata/scenario/execute.py
+++ powersimdata/scenario/execute.py
@@ -140,9 +140,9 @@ class Execute(State):
             print("--> Extracting output data on server")
             username = os.getlogin()
             cmd = ['ssh', username+'@'+const.SERVER_ADDRESS,
-                   'export PYTHONPATH="/home/EGM/v2/PostREISE/:$PYTHONPATH";',
+                   'export PYTHONPATH="/home/EGM/v2/REISE/utility/:$PYTHONPATH";',
                    'python3',
-                   '/home/EGM/v2/PostREISE/postreise/extract/extract_data.py',
+                   '/home/EGM/v2/REISE/utility/extract_data.py',
                    self._scenario_info['id']]
             process = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
             print("PID: %s" % process.pid)
