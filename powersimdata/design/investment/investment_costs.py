fe919a9f1b2fd5126acf492eca41f26018293e29
Nina <nina.vincent@breakthroughenergy.org>
2020-09-17 18:43:56 -0700

    style: format to black, and update function names to lowercase

--- powersimdata/design/investment/investment_costs.py
+++ powersimdata/design/investment/investment_costs.py
@@ -1,15 +1,16 @@
 import copy as cp
 import os
-import pandas as pd
-import numpy as np
 
+import numpy as np
+import pandas as pd
+
+from powersimdata.design.investment.create_mapping_files import (
+    bus_to_neem_reg,
+    plant_to_reeds_reg,
+)
+from powersimdata.input.grid import Grid
 from powersimdata.scenario.scenario import Scenario
 from powersimdata.utility.distance import haversine
-from powersimdata.input.grid import Grid
-from powersimdata.design.investment.create_mapping_files import (
-    plant_to_ReEDS_reg,
-    bus_to_NEEM_reg,
-    )
 
 
 def calculate_ac_inv_costs(scenario, year):
@@ -54,13 +55,13 @@ def _calculate_ac_inv_costs(grid_new, year):
     :return: (*dict*) -- Total costs (line costs, transformer costs) (in $2010).
     """
 
-    def select_kV(x, cost_df):
-        ''' Given a single branch, determine the closest kV category for cost purposes.
+    def select_kv(x, cost_df):
+        """Given a single branch, determine the closest kV category for cost purposes.
         :param pandas.core.series.Series x: data for a single branch.
         :param pandas.core.frame.DataFrame cost_df: DataFrame with kV, MW, cost columns.
         :return: (*numpy.int64*) -- kV_cost (closest kV) to be assigned to given branch.
-        '''
-        return cost_df.loc[np.argmin(np.abs(cost_df['kV_cost'] - x.kV)), 'kV_cost']
+        """
+        return cost_df.loc[np.argmin(np.abs(cost_df["kV_cost"] - x.kV)), "kV_cost"]
 
     def select_mw(x, cost_df):
         """Given a single branch, determine the closest kV/MW combination and return
@@ -72,27 +73,26 @@ def _calculate_ac_inv_costs(grid_new, year):
             assigned to given branch
         """
 
-        #select corresponding cost table of selected kV
-        tmp = cost_df[cost_df['kV_cost'] == x.kV_cost]
-        #get rid of NaN values in this kV table
-        tmp = tmp[~tmp['MW'].isna()]
-        #find closest MW & corresponding cost
-        return tmp.iloc[np.argmin(np.abs(tmp['MW'] - x.rateA))][['MW', 'costMWmi']]
+        # select corresponding cost table of selected kV
+        tmp = cost_df[cost_df["kV_cost"] == x.kV_cost]
+        # get rid of NaN values in this kV table
+        tmp = tmp[~tmp["MW"].isna()]
+        # find closest MW & corresponding cost
+        return tmp.iloc[np.argmin(np.abs(tmp["MW"] - x.rateA))][["MW", "costMWmi"]]
 
     if isinstance(year, (int, str)):
         year = int(year)
-        if year not in range(2020,2051):
+        if year not in range(2020, 2051):
             raise ValueError("year not in range.")
     else:
         raise TypeError("year must be int or str.")
 
-
-    DIR = os.path.join(os.path.dirname(__file__), "Data")
+    data_dir = os.path.join(os.path.dirname(__file__), "Data")
 
     # import data
-    ac_cost = pd.read_csv(os.path.join(DIR, "LineBase.csv"))
-    ac_reg_mult = pd.read_csv(os.path.join(DIR, "LineRegMult.csv"))
-    xfmr_cost = pd.read_csv(os.path.join(DIR, "Transformers.csv"))
+    ac_cost = pd.read_csv(os.path.join(data_dir, "LineBase.csv"))
+    ac_reg_mult = pd.read_csv(os.path.join(data_dir, "LineRegMult.csv"))
+    xfmr_cost = pd.read_csv(os.path.join(data_dir, "Transformers.csv"))
 
     # map line kV
     bus = grid_new.bus
@@ -154,24 +154,27 @@ def _calculate_ac_inv_costs(grid_new, year):
     lines.loc[:, "mult"] = (lines["mult_to"] + lines["mult_from"]) / 2.0
 
     # calculate MWmi
-    lines.loc[:, 'lengthMi'] = lines.apply(lambda x: haversine((x.from_lat, x.from_lon), (x.to_lat, x.to_lon)), axis=1)
-    lines.loc[:, 'MWmi'] = lines['lengthMi'] * lines['rateA']
+    lines.loc[:, "lengthMi"] = lines.apply(
+        lambda x: haversine((x.from_lat, x.from_lon), (x.to_lat, x.to_lon)), axis=1
+    )
+    lines.loc[:, "MWmi"] = lines["lengthMi"] * lines["rateA"]
 
     # calculate cost of each line
-    lines.loc[:, 'Cost'] = lines['MWmi'] * lines['costMWmi']  * lines['mult']
-    print(lines[['rateA','reg_from','reg_to','mult_to','mult_from','mult','lengthMi','MWmi','Cost']])
+    lines.loc[:, "Cost"] = lines["MWmi"] * lines["costMWmi"] * lines["mult"]
 
     # sum of all line costs
     lines_sum = float(lines.Cost.sum())
 
     # calculate transformer costs
-    transformers.loc[:, 'kV_cost'] = transformers.apply(lambda x: select_kV(x, xfmr_cost), axis=1)
-    transformers = transformers.merge(xfmr_cost, on='kV_cost', how="left")
+    transformers.loc[:, "kV_cost"] = transformers.apply(
+        lambda x: select_kv(x, xfmr_cost), axis=1
+    )
+    transformers = transformers.merge(xfmr_cost, on="kV_cost", how="left")
 
-    #sum of all transformer costs
+    # sum of all transformer costs
     transformers_sum = float(transformers.Cost.sum())
 
-    dict1 = {"line_cost": lines_sum,"transformer_cost": transformers_sum}
+    dict1 = {"line_cost": lines_sum, "transformer_cost": transformers_sum}
     return dict1
 
 
@@ -196,6 +199,7 @@ def calculate_dc_inv_costs(scenario, year):
     costs = _calculate_dc_inv_costs(grid_new, year)
     return costs
 
+
 def _calculate_dc_inv_costs(grid_new, year):
     """Given a grid, calculate the total cost of that grid's dc line investment.
     This function is separate from calculate_dc_inv_costs() for testing purposes.
@@ -210,49 +214,56 @@ def _calculate_dc_inv_costs(grid_new, year):
 
     if isinstance(year, (int, str)):
         year = int(year)
-        if year not in range(2020,2051):
+        if year not in range(2020, 2051):
             raise ValueError("year not in range.")
     else:
         raise TypeError("year must be int or str.")
 
-    DIR = os.path.join(os.path.dirname(__file__), "Data")
+    data_dir = os.path.join(os.path.dirname(__file__), "Data")
 
     # import data
-    dc_cost = pd.read_csv(os.path.join(DIR,"HVDC.csv"))#.astype('float64')
-    dc_term_cost = pd.read_csv(os.path.join(DIR, "HVDCTerminal.csv"))#.astype('float64')
+    dc_cost = pd.read_csv(os.path.join(data_dir, "HVDC.csv"))  # .astype('float64')
+    dc_term_cost = pd.read_csv(os.path.join(data_dir, "HVDCTerminal.csv"))
 
     bus = grid_new.bus
     dcline = grid_new.dcline
 
-    #if any dclines, do calculations, otherwise, return 0 costs.
+    # if any dclines, do calculations, otherwise, return 0 costs.
     if len(dcline != 0):
         # Find line length
-        dcline['from_lat'] = dcline.apply(lambda x: bus.loc[x.from_bus_id, 'lat'], axis=1)
-        dcline['from_lon'] = dcline.apply(lambda x: bus.loc[x.from_bus_id, 'lon'], axis=1)
+        dcline["from_lat"] = dcline.apply(
+            lambda x: bus.loc[x.from_bus_id, "lat"], axis=1
+        )
+        dcline["from_lon"] = dcline.apply(
+            lambda x: bus.loc[x.from_bus_id, "lon"], axis=1
+        )
 
-        dcline['to_lat'] = dcline.apply(lambda x: bus.loc[x.to_bus_id, 'lat'], axis=1)
-        dcline['to_lon'] = dcline.apply(lambda x: bus.loc[x.to_bus_id, 'lon'], axis=1)
+        dcline["to_lat"] = dcline.apply(lambda x: bus.loc[x.to_bus_id, "lat"], axis=1)
+        dcline["to_lon"] = dcline.apply(lambda x: bus.loc[x.to_bus_id, "lon"], axis=1)
 
-        dcline['lengthMi'] = dcline.apply(lambda x: haversine((x.from_lat, x.from_lon), (x.to_lat, x.to_lon)), axis=1)
-        dcline = dcline[dcline['lengthMi'] != 0]
+        dcline["lengthMi"] = dcline.apply(
+            lambda x: haversine((x.from_lat, x.from_lon), (x.to_lat, x.to_lon)), axis=1
+        )
+        dcline = dcline[dcline["lengthMi"] != 0]
 
         # Calculate MWmi value
-        dcline['MWmi'] = dcline['lengthMi'] * dcline['Pmax']
+        dcline["MWmi"] = dcline["lengthMi"] * dcline["Pmax"]
 
         # Find $/MW-mi cost
-        dcline.loc[:, 'costMWmi'] = dc_cost['costMWmi'][0]
+        dcline.loc[:, "costMWmi"] = dc_cost["costMWmi"][0]
 
         # Find base cost (excluding terminal cost)
-        dcline['Cost'] = dcline['MWmi'] * dcline['costMWmi']
+        dcline["Cost"] = dcline["MWmi"] * dcline["costMWmi"]
 
         # Add extra terminal cost for dc
-        dcline['Cost'] += dc_term_cost['costTerm'][0]
+        dcline["Cost"] += dc_term_cost["costTerm"][0]
         # Find sum of costs over all dclines
-        costs = dcline['Cost'].sum()
+        costs = dcline["Cost"].sum()
     else:
         costs = 0
 
     return costs
+
 
 def calculate_gen_inv_costs(scenario, year, cost_case):
     """Given a Scenario object, calculate the total cost of building that scenario's
@@ -284,10 +295,11 @@ def calculate_gen_inv_costs(scenario, year, cost_case):
     # Drop small changes
     grid_new.plant = grid_new.plant[grid_new.plant.Pmax > 0.01]
 
-    costs = _calculate_gen_inv_costs( grid_new, year, cost_case)
+    costs = _calculate_gen_inv_costs(grid_new, year, cost_case)
     return costs
 
-def _calculate_gen_inv_costs(grid_new, year,cost_case):
+
+def _calculate_gen_inv_costs(grid_new, year, cost_case):
     """Given a grid, calculate the total cost of building that generation investment.
     Computes total capital cost as CAPEX_total = CAPEX ($/MW) * Pmax (MW) * reg_cap_cost_mult [regional cost multiplier]
     This function is separate from calculate_gen_inv_costs() for testing purposes.
@@ -305,8 +317,8 @@ def _calculate_gen_inv_costs(grid_new, year,cost_case):
     :return: (*pandas.DataFrame*) -- Total generation investment cost summed by technology (in $2018).
     """
 
-    def load_cost(file_name,year,cost_case,DIR):
-        '''
+    def load_cost(file_name, year, cost_case, data_dir):
+        """
         Load in base costs from NREL's 2020 ATB for generation technologies (CAPEX).
             Can be adapted in the future for FOM, VOM, & CAPEX.
         This data is pulled from the ATB xlsx file Summary pages (saved as csv's).
@@ -319,40 +331,42 @@ def _calculate_gen_inv_costs(grid_new, year,cost_case):
             'Moderate': mid cost case,
             'Conservative': generally higher costs,
             'Advanced': generally lower costs
+        :param str data_dir: the Data directory.
         :return: (*pandas.DataFrame*) -- Cost by technology/subtype (in $2018).
-        '''
+        """
         pre = "2020-ATB-Summary"
         if file_name != "":
             pre = pre + "_"
-        cost = pd.read_csv(os.path.join(DIR,pre+file_name+".csv"))
-        cost = cost.dropna(axis=0,how='all')
+        cost = pd.read_csv(os.path.join(data_dir, pre + file_name + ".csv"))
+        cost = cost.dropna(axis=0, how="all")
 
-        #drop non-useful columns
-        cols_drop = cost.columns[~cost.columns.isin(
-                [str(x) for x in cost.columns[0:6]]+ ["Metric",str(year)])]
-        cost.drop(cols_drop,axis=1,inplace=True)
+        # drop non-useful columns
+        cols_drop = cost.columns[
+            ~cost.columns.isin(
+                [str(x) for x in cost.columns[0:6]] + ["Metric", str(year)]
+            )
+        ]
+        cost.drop(cols_drop, axis=1, inplace=True)
 
-        #rename year of interest column
-        cost.rename(columns = {str(year): "value"},
-              inplace = True)
+        # rename year of interest column
+        cost.rename(columns={str(year): "value"}, inplace=True)
 
-        #get rid of #refs
-        cost.drop(cost[cost['value'] == '#REF!'].index,inplace=True)
+        # get rid of #refs
+        cost.drop(cost[cost["value"] == "#REF!"].index, inplace=True)
 
-        #get rid of $s, commas
-        cost['value'] = cost['value'].str.replace('$', '')
-        cost['value'] = cost['value'].str.replace(',', '').astype('float64')
+        # get rid of $s, commas
+        cost["value"] = cost["value"].str.replace("$", "")
+        cost["value"] = cost["value"].str.replace(",", "").astype("float64")
 
-        #scale from $/kW to $/MW (for CAPEX + FOM)
-        if file_name in ['CAPEX','FOM']:
-            cost['value'] = 1000 * cost['value']
+        # scale from $/kW to $/MW (for CAPEX + FOM)
+        if file_name in ["CAPEX", "FOM"]:
+            cost["value"] = 1000 * cost["value"]
 
-        cost.rename(columns = {'value': file_name},
-              inplace = True)
+        cost.rename(columns={"value": file_name}, inplace=True)
 
-        #select scenario of interest
-        cost = cost[cost['CostCase']==cost_case]
-        cost.drop(['CostCase'],axis=1,inplace=True)
+        # select scenario of interest
+        cost = cost[cost["CostCase"] == cost_case]
+        cost.drop(["CostCase"], axis=1, inplace=True)
 
         return cost
 
@@ -369,49 +383,148 @@ def _calculate_gen_inv_costs(grid_new, year,cost_case):
     else:
         raise TypeError("cost_case must be str.")
 
-    DIR = os.path.join(os.path.dirname(__file__), "Data")
+    data_dir = os.path.join(os.path.dirname(__file__), "Data")
 
     plants = grid_new.plant
-    plants = plants[~plants.type.isin(['dfo', 'other'])]  # drop these technologies, no cost data
+    plants = plants[
+        ~plants.type.isin(["dfo", "other"])
+    ]  # drop these technologies, no cost data
 
     # BASE TECHNOLOGY COST
 
-    #load in investment costs $/MW
-    gen_costs = load_cost("CAPEX",year, cost_case, DIR)
+    # load in investment costs $/MW
+    gen_costs = load_cost("CAPEX", year, cost_case, data_dir)
     # keep only certain (arbutrary) subclasses for now
-    gen_costs = gen_costs[ gen_costs['TechDetail'].isin(['HydroFlash','NPD1','newAvgCF','Class1' ,'CCAvgCF','OTRG1','LTRG1','4Hr Battery Storage','Seattle'])] #only keep HydroFlash for geothermal
-    #rename techs to match grid object
-    gen_costs.replace(['OffShoreWind','LandbasedWind','UtilityPV','Battery','CSP','NaturalGas','Hydropower','Nuclear','Biopower','Geothermal','Coal'], ['wind_offshore','wind','solar','storage','csp','ng','hydro','nuclear','bio','geothermal','coal'],inplace=True)
-    gen_costs.drop(['Key','FinancialCase','CRPYears'],axis=1,inplace=True)
-    #ATB technology costs merge
-    plants = plants.merge(gen_costs,right_on='Technology',left_on='type',how='left')
+    gen_costs = gen_costs[
+        gen_costs["TechDetail"].isin(
+            [
+                "HydroFlash",
+                "NPD1",
+                "newAvgCF",
+                "Class1",
+                "CCAvgCF",
+                "OTRG1",
+                "LTRG1",
+                "4Hr Battery Storage",
+                "Seattle",
+            ]
+        )
+    ]  # only keep HydroFlash for geothermal
+    # rename techs to match grid object
+    gen_costs.replace(
+        [
+            "OffShoreWind",
+            "LandbasedWind",
+            "UtilityPV",
+            "Battery",
+            "CSP",
+            "NaturalGas",
+            "Hydropower",
+            "Nuclear",
+            "Biopower",
+            "Geothermal",
+            "Coal",
+        ],
+        [
+            "wind_offshore",
+            "wind",
+            "solar",
+            "storage",
+            "csp",
+            "ng",
+            "hydro",
+            "nuclear",
+            "bio",
+            "geothermal",
+            "coal",
+        ],
+        inplace=True,
+    )
+    gen_costs.drop(["Key", "FinancialCase", "CRPYears"], axis=1, inplace=True)
+    # ATB technology costs merge
+    plants = plants.merge(gen_costs, right_on="Technology", left_on="type", how="left")
 
     # REGIONAL COST MULTIPLIER
 
     # Find ReEDS regions of plants (for regional cost multipliers)
-    pts_plant = plant_to_ReEDS_reg(plants,DIR)
+    pts_plant = plant_to_reeds_reg(plants, data_dir)
     plants = plants.merge(pts_plant, left_index=True, right_on="plant_id", how="left")
 
-    #keep region 'r' as wind region 'rs' if tech is wind, 'rb' ba region is tech is solar or battery
-    plants.loc[:,'r'] = ''
-    rs_tech = ['wind','wind_offshore','csp'] #wind regions (rs) (apply to wind and csp)
-    plants.loc[plants['type'].isin(rs_tech),'r'] = plants.loc[plants['type'].isin(rs_tech),'rs']
-    rb_tech =['solar','storage','nuclear','coal','ng','hydro','geothermal']  #BA regions (rb) (apply to rest of techs)
-    plants.loc[plants['type'].isin(rb_tech),'r'] = plants.loc[plants['type'].isin(rb_tech),'rb']
-    plants.drop(['rs','rb'],axis=1,inplace=True)
+    # keep region 'r' as wind region 'rs' if tech is wind, 'rb' ba region is tech is solar or battery
+    plants.loc[:, "r"] = ""
+    rs_tech = [
+        "wind",
+        "wind_offshore",
+        "csp",
+    ]  # wind regions (rs) (apply to wind and csp)
+    plants.loc[plants["type"].isin(rs_tech), "r"] = plants.loc[
+        plants["type"].isin(rs_tech), "rs"
+    ]
+    rb_tech = [
+        "solar",
+        "storage",
+        "nuclear",
+        "coal",
+        "ng",
+        "hydro",
+        "geothermal",
+    ]  # BA regions (rb) (apply to rest of techs)
+    plants.loc[plants["type"].isin(rb_tech), "r"] = plants.loc[
+        plants["type"].isin(rb_tech), "rb"
+    ]
+    plants.drop(["rs", "rb"], axis=1, inplace=True)
 
     # merge regional multipliers with plants
     region_multiplier = pd.read_csv("in/reg_cap_cost_mult_default.csv")
-    region_multiplier = region_multiplier[ region_multiplier['i'].isin(['wind-ofs_1','wind-ons_1','upv_1','battery','coal-new','Gas-CC','Hydro','Nuclear','geothermal']) ]
-    region_multiplier.replace(['wind-ofs_1','wind-ons_1','upv_1','battery','Gas-CC','Nuclear','Hydro','coal-new','csp-ns'],['wind_offshore','wind','solar','storage','ng','nuclear','hydro','coal','csp'],inplace=True)
-    plants = plants.merge(region_multiplier,left_on=['r','Technology'],right_on=['r','i'],how='left')
+    region_multiplier = region_multiplier[
+        region_multiplier["i"].isin(
+            [
+                "wind-ofs_1",
+                "wind-ons_1",
+                "upv_1",
+                "battery",
+                "coal-new",
+                "Gas-CC",
+                "Hydro",
+                "Nuclear",
+                "geothermal",
+            ]
+        )
+    ]
+    region_multiplier.replace(
+        [
+            "wind-ofs_1",
+            "wind-ons_1",
+            "upv_1",
+            "battery",
+            "Gas-CC",
+            "Nuclear",
+            "Hydro",
+            "coal-new",
+            "csp-ns",
+        ],
+        [
+            "wind_offshore",
+            "wind",
+            "solar",
+            "storage",
+            "ng",
+            "nuclear",
+            "hydro",
+            "coal",
+            "csp",
+        ],
+        inplace=True,
+    )
+    plants = plants.merge(
+        region_multiplier, left_on=["r", "Technology"], right_on=["r", "i"], how="left"
+    )
 
-    #multiply all together to get summed CAPEX ($)
-    plants.loc[:,'CAPEX_total'] = plants['CAPEX'] * plants['Pmax'] * plants['reg_cap_cost_mult']
+    # multiply all together to get summed CAPEX ($)
+    plants.loc[:, "CAPEX_total"] = (
+        plants["CAPEX"] * plants["Pmax"] * plants["reg_cap_cost_mult"]
+    )
 
-    #sum cost by technology
-    tech_sum = plants.groupby(['Technology'])['CAPEX_total'].sum()
+    # sum cost by technology
+    tech_sum = plants.groupby(["Technology"])["CAPEX_total"].sum()
     return tech_sum
-
-
-
