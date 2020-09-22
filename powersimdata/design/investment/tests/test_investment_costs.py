18fd824598f68eeb92bb0179512ecf6fb5cf85cd
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2020-09-22 15:45:49 -0700

    refactor: change tests to not need to map buses to regions

--- powersimdata/design/investment/tests/test_investment_costs.py
+++ powersimdata/design/investment/tests/test_investment_costs.py
@@ -10,33 +10,40 @@ from powersimdata.design.investment.investment_costs import (
     _calculate_gen_inv_costs,
 )
 from powersimdata.tests.mock_grid import MockGrid
-from powersimdata.tests.mock_scenario import MockScenario
+
+# bus_id is the index
+mock_bus = {
+    "bus_id": [2010228, 2021106, 2010319, 2010320],
+    "lat": [47.6146, 37.7849, 47.6408, 47.6408],
+    "lon": [-122.326, -122.407, -122.339, -122.339],
+    "baseKV": [100, 346, 230, 800],
+}
 
 # branch 10-12 from Seattle (s3, p1, NWPP Coal) to San Francisco (s25, p9, NP15) (~679 miles)
 # branch 13-14 are transformers (0 miles)
 mock_branch = {
     "branch_id": [10, 11, 12, 13, 14],
     "rateA": [0, 10, 1100, 30, 40],
-    "from_lat": [47.61, 47.61, 47.61, 47.61, 47.61],
-    "from_lon": [-122.33, -122.33, -122.33, -122.33, -122.33],
-    "to_lat": [37.78, 37.78, 37.78, 47.61, 47.61],
-    "to_lon": [-122.42, -122.42, -122.42, -122.33, -122.33],
-    "from_bus_id": [1, 1, 3, 3, 4],
-    "to_bus_id": [2, 2, 2, 3, 4],
+    "from_bus_id": [2010228, 2010228, 2010319, 2010319, 2010320],
+    "to_bus_id": [2021106, 2021106, 2021106, 2010320, 2010319],
     "branch_device_type": 3 * ["Line"] + 2 * ["Transformer"],
 }
-
-# bus_id is the index
-mock_bus = {
-    "bus_id": [1, 2, 3, 4],
-    "lat": [47.61, 37.78, 47.61, 47.61],
-    "lon": [-122.33, -122.42, -122.42, -122.42],
-    "baseKV": [100, 346, 230, 800],
-}
+mock_branch["from_lat"] = [
+    mock_bus["lat"][mock_bus["bus_id"].index(bus)] for bus in mock_branch["from_bus_id"]
+]
+mock_branch["from_lon"] = [
+    mock_bus["lon"][mock_bus["bus_id"].index(bus)] for bus in mock_branch["from_bus_id"]
+]
+mock_branch["to_lat"] = [
+    mock_bus["lat"][mock_bus["bus_id"].index(bus)] for bus in mock_branch["to_bus_id"]
+]
+mock_branch["to_lon"] = [
+    mock_bus["lon"][mock_bus["bus_id"].index(bus)] for bus in mock_branch["to_bus_id"]
+]
 
 mock_plant = {
     "plant_id": ["A", "B", "C", "D", "E", "F", "G"],
-    "bus_id": [1, 1, 2, 3, 3, 3, 4],
+    "bus_id": [2010228, 2010228, 2021106, 2010319, 2010319, 2010319, 2010320],
     "type": ["solar", "coal", "wind", "solar", "solar", "ng", "wind"],
     "Pmax": [15, 30, 10, 12, 8, 20, 15],
 }
@@ -44,8 +51,8 @@ mock_plant = {
 mock_dcline = {
     "dcline_id": [5],
     "Pmax": [10],
-    "from_bus_id": [1],
-    "to_bus_id": [2],
+    "from_bus_id": [2010228],
+    "to_bus_id": [2021106],
 }
 
 grid_attrs = {
@@ -82,10 +89,9 @@ class TestCalculateACInvCosts(unittest.TestCase):
 
     def test_calculate_ac_inv_costs(self):
         expected_ac_cost = {
-            "line_cost": (679.2035515226485 * (1 + 2.25) / 2.0)
-            * (
-                0 + 3666.67 * 10 + 1500 * 1100
-            ),  # (miles * (reg_mult1 + reg_mult2)/2 ) * (basecost * rateA)
+            # ((reg_mult1 + reg_mult2) / 2) * sum(basecost * rateA * miles)
+            "line_cost": ((1 + 2.25) / 2)
+            * (3666.67 * 10 * 679.179925842 + 1500 * 1100 * 680.986501516),
             "transformer_cost": 5500000 + 42500000,
         }
         ac_cost = _calculate_ac_inv_costs(self.grid, "2025")
@@ -97,7 +103,7 @@ class TestCalculateDCInvCosts(unittest.TestCase):
         self.grid = MockGrid(grid_attrs)
 
     def test_calculate_dc_inv_costs(self):
-        expected_dc_cost = 10 * 679.2035515226485 * 457.1428571 + 550000000
+        expected_dc_cost = 10 * 679.1799258421203 * 457.1428571 + 550000000
         dc_cost = _calculate_dc_inv_costs(self.grid, "2025")
         self.assertAlmostEqual(
             dc_cost,
