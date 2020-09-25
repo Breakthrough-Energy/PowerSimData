cc47277914fe92e5024b032372cbe4e8817ca83a
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2020-09-24 17:54:51 -0700

    refactor: change investment tests, unittest -> pytest

--- powersimdata/design/investment/tests/test_investment_costs.py
+++ powersimdata/design/investment/tests/test_investment_costs.py
@@ -1,8 +1,4 @@
-import unittest
-
-import pandas as pd
-from numpy.testing import assert_array_almost_equal, assert_array_equal
-from pandas.testing import assert_frame_equal, assert_series_equal
+import pytest
 
 from powersimdata.design.investment.investment_costs import (
     _calculate_ac_inv_costs,
@@ -63,52 +59,25 @@ grid_attrs = {
     "dcline": mock_dcline,
 }
 
-
-class TestCalculateACInvCosts(unittest.TestCase):
-    def setUp(self):
-        self.grid = MockGrid(grid_attrs)
-        self.expected_keys = {"line_cost", "transformer_cost"}
-
-    def _check_expected_values(self, ac_cost, expected_ac_cost):
-        """Check for proper structure and that values match expected.
-
-        :param dict ac_cost: dict of investment cost.
-        :param dict expected_ac_cost: dict of expected investment cost.
-        """
-
-        self.assertIsInstance(ac_cost, dict, "dict not returned")
-        self.assertEqual(
-            self.expected_keys, ac_cost.keys(), msg="Dict keys not as expected"
-        )
-        for v in ac_cost.values():
-            self.assertIsInstance(v, (float, int))
-        for k in expected_ac_cost.keys():
-            err_msg = "Did not get expected value for " + str(k)
-            self.assertAlmostEqual(
-                ac_cost[k], expected_ac_cost[k], places=2, msg=err_msg
-            )
-
-    def test_calculate_ac_inv_costs(self):
-        expected_ac_cost = {
-            # ((reg_mult1 + reg_mult2) / 2) * sum(basecost * rateA * miles)
-            "line_cost": ((1 + 2.25) / 2)
-            * (3666.67 * 10 * 679.179925842 + 1500 * 1100 * 680.986501516),
-            "transformer_cost": 5500000 + 42500000,
-        }
-        ac_cost = _calculate_ac_inv_costs(self.grid, "2025")
-        self._check_expected_values(ac_cost, expected_ac_cost)
+@pytest.fixture
+def mock_grid():
+    return MockGrid(grid_attrs)
 
 
-class TestCalculateDCInvCosts(unittest.TestCase):
-    def setUp(self):
-        self.grid = MockGrid(grid_attrs)
+def test_calculate_ac_inv_costs(mock_grid):
+    expected_ac_cost = {
+        # ((reg_mult1 + reg_mult2) / 2) * sum(basecost * rateA * miles)
+        "line_cost": ((1 + 2.25) / 2)
+        * (3666.67 * 10 * 679.179925842 + 1500 * 1100 * 680.986501516),
+        "transformer_cost": 5500000 + 42500000,
+    }
+    ac_cost = _calculate_ac_inv_costs(mock_grid, "2025")
+    assert ac_cost.keys() == expected_ac_cost.keys()
+    for k in ac_cost.keys():
+        assert ac_cost[k] == pytest.approx(expected_ac_cost[k])
 
-    def test_calculate_dc_inv_costs(self):
-        expected_dc_cost = 10 * 679.1799258421203 * 457.1428571 + 550000000
-        dc_cost = _calculate_dc_inv_costs(self.grid, "2025")
-        self.assertAlmostEqual(
-            dc_cost,
-            expected_dc_cost,
-            places=2,
-            msg="Did not get expected value for dc_cost.",
-        )
+
+def test_calculate_dc_inv_costs(mock_grid):
+    expected_dc_cost = 10 * 679.1799258421203 * 457.1428571 + 550000000
+    dc_cost = _calculate_dc_inv_costs(mock_grid, "2025")
+    assert dc_cost == pytest.approx(expected_dc_cost)
