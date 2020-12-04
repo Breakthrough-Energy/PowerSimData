a6b8cadd5e11ca11b8142dc5c157f56960457eda
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2020-12-03 16:09:05 -0800

    fixup! refactor: remove 'all' clear option (redundant with None)

--- powersimdata/input/change_table.py
+++ powersimdata/input/change_table.py
@@ -182,7 +182,7 @@ class ChangeTable(object):
         if isinstance(which, str):
             which = {which}
         if not isinstance(which, set):
-            raise TypeError("Which must be a str, a set, or None (defaults to 'all')")
+            raise TypeError("Which must be a str, a set, or None (defaults to all)")
         if not which <= allowed:
             raise ValueError("which must contain only: " + " | ".join(allowed))
         # Clear only top-level keys specified in which
