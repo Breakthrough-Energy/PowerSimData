4b0e1e058beedad9fc0d9e098495348d893879d0
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2020-12-03 19:33:30 -0800

    test: add test for adding elements at new buses

--- powersimdata/input/tests/test_change_table.py
+++ powersimdata/input/tests/test_change_table.py
@@ -410,6 +410,21 @@ def test_add_bus_bad_type(ct):
         ct.add_bus({"bus1": {"lat": 40, "lon": 50.5, "zone_id": 2}})
 
 
+def test_add_new_elements_at_new_buses(ct):
+    max_existing_index = grid.bus.index.max()
+    new_buses = [
+        {"lat": 40, "lon": 50.5, "zone_id": 2, "baseKV": 69},
+        {"lat": -40.5, "lon": -50, "zone_name": "Massachusetts", "Pd": 10},
+    ]
+    ct.add_bus(new_buses)
+    new_bus1 = max_existing_index + 1
+    new_bus2 = max_existing_index + 2
+    ct.add_storage_capacity(bus_id={new_bus1: 100})
+    ct.add_dcline([{"from_bus_id": new_bus1, "to_bus_id": new_bus2, "capacity": 200}])
+    ct.add_branch([{"from_bus_id": new_bus1, "to_bus_id": new_bus2, "capacity": 300}])
+    ct.add_plant([{"type": "wind", "bus_id": new_bus2, "Pmax": 400}])
+
+
 def test_change_table_clear_success(ct):
     fake_scaling = {"demand", "branch", "solar", "ng_cost", "coal_pmin", "dcline"}
     fake_additions = {"storage", "new_dcline", "new_branch", "new_plant"}
