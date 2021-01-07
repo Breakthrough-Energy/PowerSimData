53fc995204010447f281cf51ec088d7acc63a855
Jon Hagg <jon.hagg@breakthroughenergy.org>
2021-01-06 17:29:33 -0800

    docs: add docstring for context

--- powersimdata/data_access/context.py
+++ powersimdata/data_access/context.py
@@ -3,8 +3,17 @@ from powersimdata.utility import server_setup
 
 
 class Context:
+    """Factory for data access instances"""
+
     @staticmethod
     def get_data_access(data_loc=None):
+        """Return a data access instance appropriate for the current
+        environment. Currently only supports internal client server mode.
+
+        :param str data_loc: pass "disk" if using for backups otherwise leave
+            the default and the behavior will be determined by environment
+            variables
+        """
         if data_loc == "disk":
             root = server_setup.BACKUP_DATA_ROOT_DIR
         else:
