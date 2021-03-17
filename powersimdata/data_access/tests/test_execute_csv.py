e60960803064cf0d447fb88f3e38adccecae923a
Jon Hagg <jon.hagg@breakthroughenergy.org>
2021-03-17 14:36:34 -0700

    chore: simplify import

--- powersimdata/data_access/tests/test_execute_csv.py
+++ powersimdata/data_access/tests/test_execute_csv.py
@@ -7,10 +7,9 @@ import pytest
 from numpy.testing import assert_array_equal
 from pandas.testing import assert_frame_equal
 
-import powersimdata.utility.templates as templates
 from powersimdata.data_access.data_access import LocalDataAccess, SSHDataAccess
 from powersimdata.data_access.execute_list import ExecuteListManager
-from powersimdata.utility import server_setup
+from powersimdata.utility import server_setup, templates
 
 
 @pytest.fixture
