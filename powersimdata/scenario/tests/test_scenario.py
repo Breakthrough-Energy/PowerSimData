d5628d2700fd60da3674b1b58770939fb7266949
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2021-04-05 11:44:12 -0700

    test: add test for Scenario init with bad descriptor

---
+++ powersimdata/scenario/tests/test_scenario.py
@@ -0,0 +1,10 @@
+import pytest
+
+from powersimdata.scenario.scenario import Scenario
+
+
+@pytest.mark.ssh
+def test_bad_scenario_name():
+    # This test will fail if we do add a scenario with this name
+    with pytest.raises(ValueError):
+        Scenario("this_scenario_does_not_exist")
