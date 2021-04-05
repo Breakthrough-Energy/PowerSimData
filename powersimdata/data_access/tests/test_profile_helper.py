b38d458b8ac1f661d48571e3b63e42d6dbce074a
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2021-04-05 11:53:14 -0700

    fix: use os-specific path join for testing

--- powersimdata/data_access/tests/test_profile_helper.py
+++ powersimdata/data_access/tests/test_profile_helper.py
@@ -1,3 +1,5 @@
+import os
+
 from powersimdata.data_access.profile_helper import ProfileHelper
 
 
@@ -21,4 +23,4 @@ def test_get_file_components():
     s_info = {"base_wind": "v8", "grid_model": "europe"}
     file_name, from_dir = ProfileHelper.get_file_components(s_info, "wind")
     assert "wind_v8.csv" == file_name
-    assert "raw/europe" == from_dir
+    assert os.path.join("raw", "europe") == from_dir
