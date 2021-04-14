c1131b1a6be630a3aaf39f2ca16631740853b2c2
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2021-04-14 12:39:37 -0700

    chore: copy TAMU init to HIFLD

--- powersimdata/network/hifld/model.py
+++ powersimdata/network/hifld/model.py
@@ -3,8 +3,12 @@ from powersimdata.input.abstract_grid import AbstractGrid
 
 class HIFLD(AbstractGrid):
     def __init__(self, interconnect):
-        # Placeholder for now
-        pass
+        """Constructor."""
+        super().__init__()
+        self._set_data_loc()
+
+        self.interconnect = check_and_format_interconnect(interconnect)
+        self._build_network()
 
 
 def check_and_format_interconnect(interconnect):
