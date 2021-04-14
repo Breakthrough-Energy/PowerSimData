122c004d97f8fd5e4a3bf403a6f5bff46611e2d0
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2021-04-14 12:18:27 -0700

    chore: add 'model' module to hifld with placeholder functions

---
+++ powersimdata/network/hifld/model.py
@@ -0,0 +1,14 @@
+class HIFLD:
+    def __init__(self, interconnect):
+        # Placeholder for now
+        pass
+
+
+def check_and_format_interconnect(interconnect):
+    # Placeholder for now
+    return interconnect
+
+
+def interconnect_to_name(interconnect):
+    # Placeholder for now
+    return interconnect
