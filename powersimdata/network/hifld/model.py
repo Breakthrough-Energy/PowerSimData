0ae44d5968cd9daaa10293323409d392b79d6a37
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2021-04-14 12:40:09 -0700

    chore: copy TAMU._set_data_loc to HIFLD

--- powersimdata/network/hifld/model.py
+++ powersimdata/network/hifld/model.py
@@ -1,3 +1,5 @@
+import os
+
 from powersimdata.input.abstract_grid import AbstractGrid
 
 
@@ -9,6 +11,18 @@ class HIFLD(AbstractGrid):
 
         self.interconnect = check_and_format_interconnect(interconnect)
         self._build_network()
+
+    def _set_data_loc(self):
+        """Sets data location.
+
+        :raises IOError: if directory does not exist.
+        """
+        top_dirname = os.path.dirname(__file__)
+        data_loc = os.path.join(top_dirname, "data")
+        if os.path.isdir(data_loc) is False:
+            raise IOError("%s directory not found" % data_loc)
+        else:
+            self.data_loc = data_loc
 
 
 def check_and_format_interconnect(interconnect):
