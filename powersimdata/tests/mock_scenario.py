0d79412fe5cba568c609b5fe2a662eb1f8f2cfcd
Daniel Olsen <daniel.olsen@breakthroughenergy.org>
2021-06-15 16:06:42 -0700

    feat: add logic to access state attributes without .state

--- powersimdata/tests/mock_scenario.py
+++ powersimdata/tests/mock_scenario.py
@@ -3,12 +3,16 @@ from powersimdata.tests.mock_analyze import MockAnalyze
 
 
 class MockScenario:
-    def __init__(self, grid_attrs=None, **kwargs):
-        """Constructor.
+    """
+    :param dict grid_attrs: fields to be added to grid.
+    :param \\*\\*kwargs: collected keyword arguments to be passed to
+        MockAnalyze init.
+    """
 
-        :param dict grid_attrs: fields to be added to grid.
-        :param pandas.DataFrame pg: dummy pg
-        """
+    _setattr_allowlist = {"state", "info"}
+
+    def __init__(self, grid_attrs=None, **kwargs):
+        """Constructor."""
         self.state = MockAnalyze(grid_attrs, **kwargs)
         self.info = {
             "id": "111",
@@ -34,3 +38,26 @@ class MockScenario:
     def __class__(self):
         """If anyone asks, I'm a Scenario object!"""
         return Scenario
+
+    def __dir__(self):
+        return sorted(super().__dir__() + list(self.state.exported_methods))
+
+    def __getattr__(self, name):
+        if name in self.state.exported_methods:
+            return getattr(self.state, name)
+        elif hasattr(self.state, "__getattr__"):
+            return self.state.__getattr__(name)
+        else:
+            raise AttributeError(
+                f"Scenario object in {self.state.name} state "
+                f"has no attribute {name}"
+            )
+
+    def __setattr__(self, name, value):
+        if name in self._setattr_allowlist:
+            super().__setattr__(name, value)
+        elif name in self.state.exported_methods:
+            raise AttributeError(
+                f"{name} is exported from Scenario.state, edit it there if necessary"
+            )
+        super().__setattr__(name, value)
