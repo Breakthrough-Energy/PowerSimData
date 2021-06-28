85d21322c7f113ace3d67f9494631ae2a5587afc
Jon Hagg <jon.hagg@breakthroughenergy.org>
2021-06-28 15:40:09 -0700

    feat: deduplicate interconnect passed to grid constructor

--- powersimdata/network/usa_tamu/tests/test_model.py
+++ powersimdata/network/usa_tamu/tests/test_model.py
@@ -3,8 +3,12 @@ import pytest
 from powersimdata.network.usa_tamu.model import TAMU, check_and_format_interconnect
 
 
+def _assert_lists_equal(a, b):
+    assert sorted(a) == sorted(b)
+
+
 def test_interconnect_type():
-    interconnect = {"Western", "Texas"}
+    interconnect = 42
     with pytest.raises(TypeError):
         check_and_format_interconnect(interconnect)
 
@@ -17,10 +21,8 @@ def test_interconnect_value():
 
 def test_interconnect_duplicate_value():
     interconnect = ["Western", "Western", "Texas"]
-    with pytest.raises(
-        ValueError, match="List of interconnects contains duplicate values"
-    ):
-        check_and_format_interconnect(interconnect)
+    result = check_and_format_interconnect(interconnect)
+    _assert_lists_equal(["Western", "Texas"], result)
 
 
 def test_interconnect_usa_is_unique():
@@ -29,37 +31,38 @@ def test_interconnect_usa_is_unique():
         check_and_format_interconnect(interconnect)
 
 
+def test_interconnect_set():
+    result = check_and_format_interconnect({"Texas", "Eastern"})
+    _assert_lists_equal(["Eastern", "Texas"], result)
+
+
 def test_interconnect():
     arg = ("Western", ["Eastern", "Western"])
     expected = (["Western"], ["Eastern", "Western"])
     for a, e in zip(arg, expected):
-        assert check_and_format_interconnect(a) == e
+        _assert_lists_equal(check_and_format_interconnect(a), e)
+
+
+def _assert_interconnect_missing(interconnect, model):
+    assert interconnect not in model.sub.interconnect.unique()
+    assert interconnect not in model.bus2sub.interconnect.unique()
+    assert interconnect not in model.bus.interconnect.unique()
+    assert interconnect not in model.plant.interconnect.unique()
+    assert interconnect not in model.branch.interconnect.unique()
+    assert interconnect not in model.gencost["before"].interconnect.unique()
+    assert interconnect not in model.gencost["after"].interconnect.unique()
+    assert interconnect not in model.dcline.from_interconnect.unique()
+    assert interconnect not in model.dcline.to_interconnect.unique()
 
 
 def test_drop_one_interconnect():
     model = TAMU(["Western", "Texas"])
-    assert model.interconnect == ["Western", "Texas"]
-    assert "eastern" not in model.sub.interconnect.unique()
-    assert "eastern" not in model.bus2sub.interconnect.unique()
-    assert "eastern" not in model.bus.interconnect.unique()
-    assert "eastern" not in model.plant.interconnect.unique()
-    assert "eastern" not in model.branch.interconnect.unique()
-    assert "eastern" not in model.gencost["before"].interconnect.unique()
-    assert "eastern" not in model.gencost["after"].interconnect.unique()
-    assert "eastern" not in model.dcline.from_interconnect.unique()
-    assert "eastern" not in model.dcline.to_interconnect.unique()
+    _assert_lists_equal(["Western", "Texas"], model.interconnect)
+    _assert_interconnect_missing("Eastern", model)
 
 
 def test_drop_two_interconnect():
     model = TAMU(["Western"])
-    assert model.interconnect == ["Western"]
+    _assert_lists_equal(["Western"], model.interconnect)
     for interconnect in ["Eastern", "Texas"]:
-        assert interconnect not in model.sub.interconnect.unique()
-        assert interconnect not in model.bus2sub.interconnect.unique()
-        assert interconnect not in model.bus.interconnect.unique()
-        assert interconnect not in model.plant.interconnect.unique()
-        assert interconnect not in model.branch.interconnect.unique()
-        assert interconnect not in model.gencost["before"].interconnect.unique()
-        assert interconnect not in model.gencost["after"].interconnect.unique()
-        assert interconnect not in model.dcline.from_interconnect.unique()
-        assert interconnect not in model.dcline.to_interconnect.unique()
+        _assert_interconnect_missing(interconnect, model)
