a4630f32a1827df1cafdc3e5dd1b4dd5c45a1c7e
Jon Hagg <jon@breakthroughenergy.org>
2022-01-14 13:49:37 -0800

    test: _get_profile_version

--- powersimdata/data_access/tests/test_profile_helper.py
+++ powersimdata/data_access/tests/test_profile_helper.py
@@ -1,4 +1,19 @@
-from powersimdata.data_access.profile_helper import ProfileHelper
+from fs.tempfs import TempFS
+
+from powersimdata.data_access.profile_helper import ProfileHelper, _get_profile_version
+
+
+def test_get_profile_version():
+    with TempFS() as tmp_fs:
+        tfs = tmp_fs.makedirs("raw/usa_tamu", recreate=True)
+        tfs.touch("solar_vOct2022.csv")
+        tfs.touch("foo_v1.0.1.csv")
+        v_solar = _get_profile_version(tfs, "solar")
+        v_foo = _get_profile_version(tfs, "foo")
+        v_missing = _get_profile_version(tfs, "missing")
+        assert "vOct2022" == v_solar[0]
+        assert "v1.0.1" == v_foo[0]
+        assert [] == v_missing
 
 
 def test_get_file_components():
