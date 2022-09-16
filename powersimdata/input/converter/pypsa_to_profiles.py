dd9e9997ac5204e0270d9a9b563fa3060261128c
Ben RdO <ben.rouilledorfeuil@breakthroughenergy.org>
2022-09-16 00:19:54 -0700

    feat: extract profiles from pypsa network

--- powersimdata/input/converter/pypsa_to_profiles.py
+++ powersimdata/input/converter/pypsa_to_profiles.py
@@ -1,33 +1,81 @@
+import numpy as np
 import pandas as pd
-from pypsa.descriptors import get_switchable_as_dense
+import pypsa
 
 
-def get_pypsa_gen_profile(network, kind):
+def get_pypsa_gen_profile(network, profile2carrier):
     """Return hydro, solar or wind profile enclosed in a PyPSA network.
 
     :param pypsa.Network network: the Network object.
-    :param str kind: either *'hydro'*, *'solar'*, *'wind'*.
-    :return: (*pandas.DataFrame*) -- profile.
+    :param dict profile2carrier: a dictionary mapping profile type to carrier type.
+        *'hydro'*, *'solar'* and *'wind'* are valid keys. Values is a corresponding
+        set of carriers as found in the Network object.
+    :return: (*dict*) -- keys are the same ones than in ``profile2carrier``. Values
+        are profiles as data frame.
+    :raises TypeError:
+        if ``network`` is not a pypsa.components.Network object.
+        if ``profile2carrier`` is not a dict.
+        if values of ``profile2carrier`` are not an iterable.
+    :raises ValueError:
+        if keys of ``profile2carrier`` are invalid.
     """
-    p_max_pu = get_switchable_as_dense(network, "Generator", "p_max_pu")
-    p_max_pu.columns = pd.to_numeric(p_max_pu.columns, errors="ignore")
-    p_max_pu.columns.name = None
-    p_max_pu.index.name = "UTC"
+    if not isinstance(network, pypsa.components.Network):
+        raise TypeError("network must be a Network object")
+    if not isinstance(profile2carrier, dict):
+        raise TypeError("profile2carrier must be a dict")
+    if not all(isinstance(v, (list, set, tuple)) for v in profile2carrier.values()):
+        raise TypeError("values of profile2carrier must be an iterable")
+    if not set(profile2carrier).issubset({"hydro", "solar", "wind"}):
+        raise ValueError(
+            "keys of profile2carrier must be a subset of ['hydro', 'solar', 'wind']"
+        )
 
-    all_gen = network.generators.copy()
-    all_gen.index = pd.to_numeric(all_gen.index, errors="ignore")
-    all_gen.index.name = None
+    profile = {}
+    for p, c in profile2carrier.items():
+        profile[p] = pd.DataFrame()
+        for component in ["generators", "storage_units", "stores"]:
+            if hasattr(network, component):
+                carrier_in_component = getattr(network, component)
+                id_carrier = set(carrier_in_component.query("carrier==list(@c)").index)
+                for t in ["inflow", "p_max_pu"]:
+                    if t in getattr(network, component + "_t"):
+                        ts = getattr(network, component + "_t")[t]
+                        if not ts.empty:
+                            id_ts = set(ts.columns)
+                            idx = list(id_carrier.intersection(id_ts))
+                            norm = (
+                                (
+                                    ts[idx]
+                                    .max()
+                                    .combine(
+                                        carrier_in_component.loc[idx, "p_nom"],
+                                        np.maximum,
+                                    )
+                                )
+                                if t == "inflow"
+                                else 1
+                            )
+                            profile[p] = pd.concat([profile[p], ts[idx] / norm], axis=1)
+                if len(set(c) - set(carrier_in_component.carrier.unique())):
+                    continue
+                else:
+                    break
+        profile[p].rename_axis(index="UTC", columns=None, inplace=True)
 
-    gen = all_gen.query("@kind in carrier").index
-    return p_max_pu[gen] * all_gen.p_nom[gen]
+    return profile
 
 
 def get_pypsa_demand_profile(network):
     """Return demand profile enclosed in a PyPSA network.
 
     :param pypsa.Network network: the Network object.
-    :return: (*pandas.DataFrame*) -- profile.
+    :return: (*pandas.DataFrame*) -- demand profile.
+    :raises TypeError:
+        if ``network`` is not a pypsa.components.Network object.
     """
+    if not isinstance(network, pypsa.components.Network):
+        raise TypeError("network must be a Network object")
+
     if not network.loads_t.p.empty:
         demand = network.loads_t.p.copy()
     else:
@@ -38,7 +86,6 @@ def get_pypsa_demand_profile(network):
             network.buses.zone_id.dropna().astype(int), axis=1
         ).sum()
     demand.columns = pd.to_numeric(demand.columns, errors="ignore")
-    demand.columns.name = None
-    demand.index.name = "UTC"
+    demand.rename_axis(index="UTC", columns=None, inplace=True)
 
     return demand
